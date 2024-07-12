10e5640f4acece77b65ef6d1e2bd80c588a94080
gaultier-souris <gaultier.souris@gmail.com>
2024-07-12 18:41:33 +0200

    navbar mobile ok

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,9 +3,9 @@ import { FaGithubSquare, FaLinkedin } from 'react-icons/fa';
 export default function NavBar() {
   return (
     <div>
-      <h1>Gaultier Souris</h1>
-      <h2>Developpeur Front End</h2>
-      <p>
+      <h1 className="text-4xl font-black">Gaultier Souris</h1>
+      <h2 className="text-xl font-bold mt-2">Developpeur Frontend</h2>
+      <p className="text-lg leading-4 mt-2">
         Developpeur junior et passionné, je suis a la recherche d'une société
         dans laquelle m'épanouir.
       </p>
@@ -19,12 +19,12 @@ export default function NavBar() {
         </ul>
       </div>
 
-      <div>
+      <div className="mt-4">
         <ul className="flex">
-          <li>
+          <li className="text-3xl">
             <FaGithubSquare />
           </li>
-          <li>
+          <li className="text-3xl">
             <FaLinkedin />
           </li>
         </ul>
